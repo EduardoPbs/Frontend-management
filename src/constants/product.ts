aea121ad88de136439f29476927419e2223bdd5c
Eduardo Soares <eduardopbs234@outlook.com>
2024-03-12 10:36:05 -0300

    fix: Correct return from data api

--- src/constants/product.ts
+++ src/constants/product.ts
@@ -2,12 +2,9 @@ export type ProductEntity = {
     id: string;
     name: string;
     code: number;
-    stock: number;
     value: number;
     category: string;
-    status: boolean;
-    createdAt: string;
-    updatedAt: string;
-    deletedAt: string | undefined | null;
+    stock: number;
+    active: boolean;
 };
 
