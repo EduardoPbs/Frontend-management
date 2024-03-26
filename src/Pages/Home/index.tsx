c1badb92635777e8fc4c6e7eb54d1bb57dbabce1
Eduardo Soares <eduardopbs234@outlook.com>
2024-03-26 13:57:53 -0300

    fix: change home layout & page container style

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -15,7 +15,10 @@ import {
     Table,
     Spinner,
     TableContainer,
+    Tooltip,
 } from '@chakra-ui/react';
+import { AlertCircle, ArrowRightCircle } from 'lucide-react';
+import { useNavigate } from 'react-router';
 
 function OrderTable() {
     const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -45,7 +48,7 @@ function OrderTable() {
     }
 
     useEffect(() => {
-        document.title = 'Management | Home'
+        document.title = 'Management | Home';
         getOrdersData();
     }, []);
 
@@ -206,13 +209,102 @@ function ProductTable() {
 }
 
 export function Home() {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [dataProducts, setDataProducts] = useState([]);
+    const navigate = useNavigate();
+
+    async function getProductsData() {
+        try {
+            const response = await http.get('/products');
+            if (response.data) {
+                setDataProducts(response.data);
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getProductsData();
+    }, []);
+
+    if (isLoading)
+        return (
+            <div className='flex justify-center'>
+                <Spinner size='xl' color='yellow.500' />
+            </div>
+        );
+
     return (
         <PageContainer title='Home'>
             <div className='flex items-center gap-2'>
                 <Content className='w-full'>
-                    <Title variant='h2'>Notificações</Title>
+                    <Title variant='h3'>
+                        Notificações{' '}
+                        <span className='text-sm opacity-50 capitalize'>
+                            | Produtos com baixo estoque
+                        </span>
+                    </Title>
 
-                    <div></div>
+                    <div className='flex flex-col gap-2 max-h-[110px] overflow-hidden overflow-y-scroll rounded-sm'>
+                        {dataProducts &&
+                            dataProducts
+                                ?.filter((p: any) => {
+                                    return p.stock <= 10;
+                                })
+                                .map((prod: any, index: number) => {
+                                    return (
+                                        <p
+                                            key={index}
+                                            className='flex items-center justify-between gap-4 border-2 px-4 py-2 hover:border-amber-400 hover:bg-white/30 hover:cursor-default duration-100'
+                                        >
+                                            <span className='flex items-center gap-2'>
+                                                <AlertCircle className='text-orange-300' />
+                                                <span className='w-[200px]'>
+                                                    {prod.name}
+                                                </span>
+                                            </span>
+                                            <span>
+                                                Estoque:{' '}
+                                                <span
+                                                    className={
+                                                        prod.stock > 5
+                                                            ? 'text-orange-300'
+                                                            : 'text-red-500'
+                                                    }
+                                                >
+                                                    {prod.stock}
+                                                </span>
+                                            </span>
+                                            <span>
+                                                Status:{' '}
+                                                <span
+                                                    className={
+                                                        prod.active
+                                                            ? 'text-green-300'
+                                                            : 'text-yellow-500'
+                                                    }
+                                                >
+                                                    {prod.active
+                                                        ? 'Online'
+                                                        : 'Offline'}
+                                                </span>
+                                            </span>
+
+                                            <Tooltip label='Ir para produtos'>
+                                                <ArrowRightCircle 
+                                                    className='hover:cursor-pointer' 
+                                                    onClick={() => {
+                                                        navigate('/products');
+                                                    }}
+                                                />
+                                            </Tooltip>
+                                        </p>
+                                    );
+                                })}
+                    </div>
                 </Content>
             </div>
 
--- src/components/PageContainer/index.tsx
+++ src/components/PageContainer/index.tsx
@@ -1,5 +1,16 @@
+import { ArrowUpRightIcon, LogOut, UserCircle2 } from 'lucide-react';
 import { DrawerModal } from '../DrawerModal';
 import { Title } from '../Title';
+import {
+    Box,
+    Button,
+    Popover,
+    PopoverArrow,
+    PopoverBody,
+    PopoverCloseButton,
+    PopoverContent,
+    PopoverTrigger,
+} from '@chakra-ui/react';
 
 export function PageContainer({
     children,
@@ -9,14 +20,49 @@ export function PageContainer({
     title?: string;
 }) {
     return (
-        <main className='flex flex-col justify-start gap-4 px-4 py-6 bg-black h-screen text-white selection:bg-amber-700 selection:text-white'>
-            <header className='flex items-center justify-start gap-4'>
-            <DrawerModal />
+        <main className='flex flex-col justify-start gap-4 bg-black h-screen text-white selection:bg-amber-700 selection:text-white'>
+            <header className='flex items-center justify-between gap-4 bg-zinc-800 px-4 py-3'>
+                <div className='flex items-center gap-4'>
+                    <DrawerModal />
 
-                <Title>{title}</Title>
+                    <Title>{title}</Title>
+                </div>
+
+                <div className='flex items-center gap-1'>
+                    <Popover>
+                        <PopoverTrigger>
+                            <UserCircle2 className='size-9 text-amber-400 hover:text-amber-400/80 hover:cursor-pointer duration-150' />
+                        </PopoverTrigger>
+                        <PopoverContent width={150}>
+                            <PopoverArrow />
+                            <PopoverCloseButton />
+                            <PopoverBody>
+                                <Box className='flex flex-col justify-center gap-2'>
+                                    <Button
+                                        className='flex items-center gap-2'
+                                        colorScheme='yellow'
+                                        height={30}
+                                    >
+                                        Gerenciar
+                                        <ArrowUpRightIcon className='size-6' />
+                                    </Button>
+                                    <Button
+                                        height={30}
+                                        colorScheme='orange'
+                                        className='flex items-center gap-2'
+                                    >
+                                        Sair
+                                        <LogOut className='size-5' />
+                                    </Button>
+                                </Box>
+                            </PopoverBody>
+                        </PopoverContent>
+                    </Popover>
+                    <a className='hover:cursor-pointer'>Logged Username</a>
+                </div>
             </header>
 
-            <section className='flex flex-col gap-4 w-full h-fit overflow-hidden'>
+            <section className='flex flex-col gap-4 px-4 py-6 w-full h-fit overflow-hidden'>
                 {children}
             </section>
         </main>
