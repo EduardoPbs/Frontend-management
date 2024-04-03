94e4aa2577804e0e705eefaa2ed7901dbc9c7052
Eduardo Soares <eduardopbs234@outlook.com>
2024-04-03 09:26:43 -0300

    feat: add file with default styles & colors

---
+++ src/constants/styles.ts
@@ -0,0 +1,16 @@
+export const primary_white: string = '#F7F7FF';
+export const primary_black: string = '#131112';
+export const primary_red: string = '#DC211C';
+export const custom_red: string = '#F40000';
+export const primary_hover_red: string = '#C80000';
+export const light_gray: string = '#D4D4D8';
+export const dark_gray: string = '#71717A';
+export const warning_red: string = '#FF0000';
+export const agreed_green: string = '#10b981';
+export const agreed_hover_green: string = '#099C6B';
+export const border_gray: string = '#71717A80';
+
+export const round_default: string = '6px';
+
+export const table_row_hover: string =
+    'font-semibold hover:bg-light-gray/60 duration-150';
