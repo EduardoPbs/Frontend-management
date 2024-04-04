857b4adbf8da73872f4f657f6e68b1a6082bec05
Eduardo Soares <eduardopbs234@outlook.com>
2024-04-04 09:28:54 -0300

    refactor: exporting pages from Page index file

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,19 +1,21 @@
-import { Home } from './Pages/Home';
-import { Login } from './Pages/Login';
-import { Orders } from './Pages/Orders';
-import { Products } from './Pages/Products';
-import { Purchases } from './Pages/Purchases';
-import { Employees } from './Pages/Employees';
-import { OrderForm } from './Pages/Orders/OrderForm';
-import { ProductForm } from './Pages/Products/ProductForm';
-import { OrderDetail } from './Pages/Orders/OrderDetail';
-import { PurchaseForm } from './Pages/Purchases/PurchaseForm';
-import { EmployeeForm } from './Pages/Employees/EmployeeForm';
 import { ChakraProvider } from '@chakra-ui/react';
-import { EmployeeDetail } from './Pages/Employees/EmployeeDetail';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
-import { PurchaseDetail } from './Pages/Purchases/PurchaseDetail';
-import { CashRegister } from './Pages/CashRegister';
+import {
+    Home,
+    Login,
+    Orders,
+    Products,
+    Purchases,
+    OrderForm,
+    Employees,
+    ProductForm,
+    OrderDetail,
+    PurchaseForm,
+    CashRegister,
+    EmployeeForm,
+    EmployeeDetail,
+    PurchaseDetail,
+} from './Pages';
 
 function AppRoutes() {
     return (
@@ -22,11 +24,11 @@ function AppRoutes() {
                 <Routes>
                     <Route path='/' element={<Home />} />
                     <Route path='/login' element={<Login />} />
-                    
+
                     <Route path='/products' element={<Products />} />
                     <Route path='/products/new' element={<ProductForm />} />
                     <Route path='/products/edit/:id' element={<ProductForm />} />
-                    
+
                     <Route path='/orders' element={<Orders />} />
                     <Route path='/orders/new/:id' element={<OrderForm />} />
                     <Route path='/orders/:id' element={<OrderDetail />} />
---
+++ src/Pages/index.ts
@@ -0,0 +1,31 @@
+import { Home } from './Home';
+import { Login } from './Login';
+import { Orders } from './Orders';
+import { Products } from './Products';
+import { Purchases } from './Purchases';
+import { Employees } from './Employees';
+import { OrderForm } from './Orders/OrderForm';
+import { ProductForm } from './Products/ProductForm';
+import { OrderDetail } from './Orders/OrderDetail';
+import { PurchaseForm } from './Purchases/PurchaseForm';
+import { EmployeeForm } from './Employees/EmployeeForm';
+import { CashRegister } from './CashRegister';
+import { EmployeeDetail } from './Employees/EmployeeDetail';
+import { PurchaseDetail } from './Purchases/PurchaseDetail';
+
+export {
+    Home,
+    Login,
+    Orders,
+    Products,
+    Purchases,
+    Employees,
+    OrderForm,
+    ProductForm,
+    OrderDetail,
+    PurchaseForm,
+    EmployeeForm,
+    CashRegister,
+    EmployeeDetail,
+    PurchaseDetail,
+};
