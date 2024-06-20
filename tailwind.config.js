5af7cde9c33a8e0d8feacdf0d56818c0d1f965b5
Eduardo Soares <eduardopbs234@outlook.com>
2024-06-20 13:06:51 -0300

    feat: add Shadcn lib & configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,35 @@
 import {
-    custom_red,
-    light_gray,
-    warning_red,
-    primary_red,
-    primary_black,
-    round_default,
-    primary_white,
-    primary_hover_red,
-    agreed_green,
     dark_gray,
+    light_gray,
+    custom_red,
+    primary_red,
     border_gray,
+    warning_red,
+    agreed_green,
+    primary_white,
+    round_default,
+    primary_black,
+    primary_hover_red,
 } from './src/constants/styles';
 
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+    darkMode: ['class'],
+    content: [
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+    ],
+    prefix: '',
     theme: {
+        container: {
+            center: true,
+            padding: '2rem',
+            screens: {
+                '2xl': '1400px',
+            },
+        },
         extend: {
             colors: {
                 'primary-white': primary_white,
@@ -28,11 +42,61 @@ export default {
                 'light-gray': light_gray,
                 'dark-gray': dark_gray,
                 'border-gray': border_gray,
+                border: 'hsl(var(--border))',
+                input: 'hsl(var(--input))',
+                ring: 'hsl(var(--ring))',
+                background: 'hsl(var(--background))',
+                foreground: 'hsl(var(--foreground))',
+                primary: {
+                    DEFAULT: 'hsl(var(--primary))',
+                    foreground: 'hsl(var(--primary-foreground))',
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    foreground: 'hsl(var(--secondary-foreground))',
+                },
+                destructive: {
+                    DEFAULT: 'hsl(var(--destructive))',
+                    foreground: 'hsl(var(--destructive-foreground))',
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    foreground: 'hsl(var(--muted-foreground))',
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    foreground: 'hsl(var(--accent-foreground))',
+                },
+                popover: {
+                    DEFAULT: 'hsl(var(--popover))',
+                    foreground: 'hsl(var(--popover-foreground))',
+                },
+                card: {
+                    DEFAULT: 'hsl(var(--card))',
+                    foreground: 'hsl(var(--card-foreground))',
+                },
             },
             borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
                 'round-default': round_default,
+            },
+            keyframes: {
+                'accordion-down': {
+                    from: { height: '0' },
+                    to: { height: 'var(--radix-accordion-content-height)' },
+                },
+                'accordion-up': {
+                    from: { height: 'var(--radix-accordion-content-height)' },
+                    to: { height: '0' },
+                },
+            },
+            animation: {
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
             },
         },
     },
-    plugins: [require('tailwind-scrollbar-hide')],
+    plugins: [require('tailwindcss-animate')],
 };
