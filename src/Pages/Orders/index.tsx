a965d49e208046800d343b6bb4176659cba7ca52
Eduardo Soares <eduardopbs234@outlook.com>
2024-06-27 10:51:56 -0300

    feat: filter orders by employee

--- src/Pages/Orders/index.tsx
+++ src/Pages/Orders/index.tsx
@@ -3,7 +3,7 @@ import { Button } from '@/components/ui/button';
 import { Content } from '@/components/Content';
 import { useOrder } from '../../hooks/useOrder';
 import { LgSpinner } from '../../components/LgSpinner';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { PlusCircle } from 'lucide-react';
 import { IconButton } from '../../components/IconButton';
 import { useNavigate } from 'react-router';
@@ -12,9 +12,14 @@ import { PageContainer } from '../../components/PageContainer';
 import { table_row_hover } from '../../constants/styles';
 import { toFullLocaleDate } from '../../utils/toFullLocaleDate';
 import { Table, TableHeader, TableHead, TableBody, TableRow, TableCell } from '@/components/ui/table';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { useEmployee } from '@/hooks/useEmployee';
+import { EmployeeEntity } from '@/types/employee';
 
 export function Orders() {
+    const [selectedEmployee, setSelectedEmployee] = useState<string>();
     const { dataOrders, isLoading } = useOrder();
+    const { dataEmployees } = useEmployee();
 
     const navigate = useNavigate();
 
@@ -26,18 +31,44 @@ export function Orders() {
 
     return (
         <PageContainer title='Vendas'>
-            <IconButton
-                label='Nova venda'
-                className='w-fit'
-                to='new'
-                icon={PlusCircle}
-            />
+            <div className='flex items-center gap-8'>
+                <IconButton
+                    label='Nova venda'
+                    className='w-fit'
+                    to='new'
+                    icon={PlusCircle}
+                />
+
+                <Select
+                    onValueChange={(event) => {
+                        const employeeData: { id: string; nome: string; } = JSON.parse(event);
+                        setSelectedEmployee(employeeData.id);
+                    }}
+                >
+                    <SelectTrigger className='w-[180px] font-semibold'>
+                        <SelectValue placeholder="Funcionário" />
+                    </SelectTrigger>
+                    <SelectContent className='max-h-[200px] font-semibold'>
+                        <SelectItem value='false'>
+                            Tudo
+                        </SelectItem>
+                        {dataEmployees.map((employee: EmployeeEntity, index: number) => {
+                            return (
+                                <SelectItem key={index} value={JSON.stringify({ id: employee.id, nome: employee.nome })}>
+                                    {employee.nome}
+                                </SelectItem>
+                            );
+                        })}
+                    </SelectContent>
+                </Select>
+            </div>
 
             <div className='flex items-center justify-between'>
                 <Title variant='h3'>
                     Registros:{' '}
                     <span className='text-4xl text-primary-red'>
-                        {dataOrders.total}
+                        {!selectedEmployee ? dataOrders.total :
+                            dataOrders.orders.filter((order: OrderEntity) => order.funcionario_id === selectedEmployee).length}
                     </span>
                 </Title>
             </div>
@@ -54,7 +85,7 @@ export function Orders() {
                         </TableRow>
                     </TableHeader>
                     <TableBody>
-                        {dataOrders.orders &&
+                        {!selectedEmployee ? dataOrders.orders &&
                             dataOrders.orders.map((order: OrderEntity) => {
                                 return (
                                     <TableRow
@@ -84,6 +115,37 @@ export function Orders() {
                                         </TableCell>
                                     </TableRow>
                                 );
+                            }).reverse() : dataOrders.orders.map((order: OrderEntity) => {
+                                if (order.funcionario_id === selectedEmployee) {
+                                    return (
+                                        <TableRow
+                                            key={order.id}
+                                            className={table_row_hover}
+                                        >
+                                            <TableCell>{order.id.slice(0, 8)}</TableCell>
+                                            <TableCell>{order.quantidade_itens}</TableCell>
+                                            <TableCell>
+                                                {toFullLocaleDate(order.criado_em)}
+                                            </TableCell>
+                                            <TableCell>
+                                                {Number(
+                                                    order.total
+                                                ).toLocaleString('pt-br', {
+                                                    style: 'currency',
+                                                    currency: 'BRL',
+                                                })}
+                                            </TableCell>
+                                            <TableCell>
+                                                <Button
+                                                    className='w-full hover:bg-primary-hover-red'
+                                                    onClick={() => { navigate(`${order.id}`); }}
+                                                >
+                                                    Ver detalhes
+                                                </Button>
+                                            </TableCell>
+                                        </TableRow>
+                                    );
+                                }
                             }).reverse()}
                     </TableBody>
                 </Table>
